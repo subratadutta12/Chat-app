7990c5b0b6a75ab341b2426202d9ba41a54ce3d3
Subrata <subratadutta641@gmail.com>
2021-06-17 01:44:57 +0530

    Initial commit

---
+++ nodeServer/index.js
@@ -0,0 +1,29 @@
+// Node server which will handle sockt io connection
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+    }
+})
+
+const users = {};
+
+io.on('connection', socket => {
+
+    // If any new joins, let other users connected to the server know!
+    socket.on('new-user-joined', name => {
+        // console.log("New-user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+
+    // If someone sends a message, broadcast it to other people
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    })
+
+    //If someone leaves the chat let others know
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('leave', users[socket.id]);
+        delete users[socket.id];
+    })
+})
